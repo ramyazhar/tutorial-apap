98a89653c7ca6df186bfddf490e7e538f5177c64
M Ramy Azhar <azharreal14@gmail.com>
2020-11-19 14:21:35 +0700

    initialize project

---
+++ traveloke/src/main/java/apap/tutorial/traveloke/rest/Setting.java
@@ -0,0 +1,5 @@
+package apap.tutorial.traveloke.rest;
+
+public class Setting {
+    final public static String hotelUrl = "https://548e1597-6a1f-442d-bd83-6a24ed59b60e.mock.pstmn.io";
+}
---
+++ traveloke/src/main/java/apap/tutorial/traveloke/restcontroller/KamarRestController.java
@@ -0,0 +1,13 @@
+package apap.tutorial.traveloke.restcontroller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class KamarRestController {
+    @Autowired
+    private KamarRestService kamarRestService;
+}
