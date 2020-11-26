4808b48df75c6248c5935e87bc453fd9505edc3d
M Ramy Azhar <azharreal14@gmail.com>
2020-11-26 12:13:03 +0700

    initialize project

---
+++ traveloke/src/main/java/apap/tutorial/traveloke/controller/UserController.java
@@ -0,0 +1,54 @@
+//package apap.tutorial.traveloke.controller;
+//
+//import apap.tutorial.traveloke.model.UserModel;
+//import apap.tutorial.traveloke.service.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//
+//@Controller
+//@RequestMapping("/user")
+//public class UserController {
+//
+//    @Autowired
+//    private UserService userService;
+//
+//    @RequestMapping(value = "/addUser", method = RequestMethod.POST)
+//    public String addUserSubmit(
+//            @ModelAttribute UserModel user, Model model){
+//        userService.addUser(user);
+//        model.addAttribute("user", user);
+//        return "redirect:/";
+//    }
+//}
+
+package apap.tutorial.traveloke.controller;
+
+import apap.tutorial.traveloke.model.UserModel;
+import apap.tutorial.traveloke.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/addUser", method = RequestMethod.POST)
+    public String addUserSubmit(
+            @ModelAttribute UserModel user, Model model){
+        userService.addUser(user);
+        model.addAttribute("user", user);
+        return "redirect:/";
+    }
+}
+
